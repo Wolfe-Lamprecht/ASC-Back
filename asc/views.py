5a91511f98a0f64cc5387b579ff1e9153d02d928
Caitlin <caitlinlamprecht@outlook.com>
2021-03-08 10:44:02 -0600

    added url paths, views, and templates

--- asc/views.py
+++ asc/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import render
 
 # Create your views here.
+def user_list(req):
+    users = Artist.objects.all()
+    return render(req, 'asc/user_list.html', {'users': users})
+
+def user_detail(req, pk):
+    user = User.objects.get(id=pk)
+    return render(req, 'asc/user_detail.html', {'user': user})
+
+def post_list(req):
+    posts = Post.objects.all()
+    return render(req, 'asc/post_list.html', {'posts': posts})
+
